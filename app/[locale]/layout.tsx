68843718dc639ad47b3d7cfa74b3c047a092da07
Federico Pastore <federicopastore.ngp@gmail.com>
2025-12-18 01:09:44 +0100

    fix: Remove global edge runtime to support SSG

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,8 +5,6 @@ import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import "../globals.css";
 
-export const runtime = 'edge';
-
 const locales = ['en', 'es', 'fr', 'de'] as const;
 
 const geistSans = Geist({
